1bfbb44403dc087ec3c3a0ada5ef05345db9a26e
ccsjcsd <ccsjcsd@DESKTOP-46S83EE>
2018-07-31 08:34:01 -0700

    First commit

---
+++ TestMvcAndGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestMvcAndGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
